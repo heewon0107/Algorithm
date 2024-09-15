84b37e31689a354cf9d4f1fe30d76cf40126e653
Shin Hee Won <shinheewon0107@gmail.com>
2024-09-15 21:54:36 +0900

    [D2] Title: 쉬운 거스름돈, Time: 116 ms, Memory: 44,796 KB -BaekjoonHub

--- SWEA/D2/1970. 쉬운 거스름돈/쉬운 거스름돈.py
+++ SWEA/D2/1970. 쉬운 거스름돈/쉬운 거스름돈.py
@@ -1,12 +1,29 @@
-T = int(input())
-for tc in range(1, T + 1):
+for tc in range(1, int(input()) + 1):
     N = int(input())
-    bill = [0] * 8
-    num = 0
-    for x in [50000, 10000, 5000, 1000, 500, 100, 50, 10]:
-        while x <= N:
-            bill[num] += 1
-            N -= x
-        num += 1
+    bills = [0] * 8
+    while N >= 50000:
+        N -= 50000
+        bills[0] += 1
+    while N >= 10000:
+        N -= 10000
+        bills[1] += 1
+    while N >= 5000:
+        N -= 5000
+        bills[2] += 1
+    while N >= 1000:
+        N -= 1000
+        bills[3] += 1
+    while N >= 500:
+        N -= 500
+        bills[4] += 1
+    while N >= 100:
+        N -= 100
+        bills[5] += 1
+    while N >= 50:
+        N -= 50
+        bills[6] += 1
+    while N >= 10:
+        N -= 10
+        bills[7] += 1
     print(f"#{tc}")
-    print(*bill)
+    print(*bills)
