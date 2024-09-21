0d96796e886459df93b0b955992b1ddc2a778899
Shin Hee Won <shinheewon0107@gmail.com>
2024-09-21 18:38:33 +0900

    [D3] Title: 새샘이의 7-3-5 게임, Time: 1,436 ms, Memory: 54,764 KB -BaekjoonHub

---
+++ SWEA/D3/5948. 새샘이의 7－3－5 게임/새샘이의 7－3－5 게임.py
@@ -0,0 +1,17 @@
+def sasam(n, idx, total):
+    global sum_list
+    if n == 3:
+        if total not in sum_list:
+            sum_list.append(total)
+    else:
+        remains = 3 - (n+1)
+        for i in range(idx, 7 - remains):
+            sasam(n + 1, i + 1, total + arr[i])
+
+T = int(input())
+for t in range(1, T+1):
+    arr = list(map(int, input().split()))
+    sum_list = []
+    sasam(0, 0, 0)
+    sum_list.sort(reverse=True)
+    print(f"#{t} {sum_list[4]}")
