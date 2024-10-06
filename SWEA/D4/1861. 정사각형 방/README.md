636319e14ebb4177cfa97735faba81d6e2ccc196
Shin Hee Won <shinheewon0107@gmail.com>
2024-10-06 21:36:43 +0900

    [D4] Title: 정사각형 방, Time: 983 ms, Memory: 51,724 KB -BaekjoonHub

---
+++ SWEA/D4/1861. 정사각형 방/README.md
@@ -0,0 +1,15 @@
+# [D4] 정사각형 방 - 1861 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5LtJYKDzsDFAXc) 
+
+### 성능 요약
+
+메모리: 51,724 KB, 시간: 983 ms, 코드길이: 948 Bytes
+
+### 제출 일자
+
+2024-10-06 21:36
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D4/1861. 정사각형 방/정사각형 방.py
@@ -0,0 +1,44 @@
+from collections import deque
+
+
+def is_valid(r, c):
+    return 0 <= r < n and 0 <= c < n
+
+
+def dijkstra(r, c):
+    if "기저 조건":
+        pass
+    first_num = board[r][c]
+    cnt = 1
+    q = deque()
+    q.append((r, c))
+    while q:
+        r, c = q.popleft()
+
+        for dr, dc in delta:
+            nr = r + dr
+            nc = c + dc
+            if is_valid(nr, nc) and board[nr][nc] == board[r][c] + 1:
+                q.append((nr, nc))
+                cnt += 1
+
+    if cnt > result[1]:
+        result[0] = first_num
+        result[1] = cnt
+    elif cnt == result[1] and first_num < result[0]:
+        result[0] = first_num
+
+
+# 상 우 하 좌
+delta = ((-1, 0), (0, 1), (1, 0), (0, -1))
+
+t = int(input())
+for tc in range(1, t + 1):
+    n = int(input())
+    board = [list(map(int, input().split())) for _ in range(n)]
+    result = [1e9, 0]
+    for i in range(n):
+        for j in range(n):
+            dijkstra(i, j)
+
+    print(f"#{tc}", *result)
