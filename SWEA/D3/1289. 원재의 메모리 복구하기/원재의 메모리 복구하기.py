0682aee969f4db027c953b3e3479c4c39c3f2cec
Shin Hee Won <shinheewon0107@gmail.com>
2025-01-10 17:13:18 +0900

    [D3] Title: 원재의 메모리 복구하기, Time: 111 ms, Memory: 46,016 KB -BaekjoonHub

--- SWEA/D3/1289. 원재의 메모리 복구하기/원재의 메모리 복구하기.py
+++ SWEA/D3/1289. 원재의 메모리 복구하기/원재의 메모리 복구하기.py
@@ -1,15 +1,34 @@
-op = [1,0]
-T = int(input())
-for tc in range(1, T + 1):
-    memory = list(map(int, input()))
-    n = len(memory)
-    now = [0] * n
-    result = 0
-    for i in range(n):
-        if now[i] == memory[i]:
-            continue
-        else:
-            result += 1
-            for x in range(i,n):
-                now[x] = op[now[x]]
-    print(f"#{tc} {result}")
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= T; i++) {
+            int changeNum = 0;
+            String input = br.readLine();
+            String[] memory = input.split("");
+
+            // 역발상 : 모든 걸 0으로
+            for (int j = 0; j < memory.length; j++) {
+                if (memory[j].equals("0")) {
+                    continue;
+                }
+                for (int k = j; k < memory.length; k++) {
+                    if (memory[k].equals("1")) {
+                        memory[k] = "0";
+                    } else {
+                        memory[k] = "1";
+                    }
+                }
+                changeNum++;
+
+            }
+            System.out.println("#"+i+" "+changeNum);
+        }
+
+    }
+}
