a5129081c5279a94a4cb57b1c1f4e22237e797e3
Shin Hee Won <shinheewon0107@gmail.com>
2025-04-24 22:52:42 +0900

    [Bronze II] Title: 공 바꾸기, Time: 104 ms, Memory: 14228 KB -BaekjoonHub

--- 백준/Bronze/10813. 공 바꾸기/공 바꾸기.java
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.java
@@ -1,42 +1,33 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        int[] balls = new int[N];
 
-        // 공 번호 입력 1 ~ N
-        for (int i = 0; i < N; i++) {
-            balls[i] = i+1;
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[] buckets = new int[n];
+        for (int i = 0; i < n; i++) {
+            buckets[i] = i + 1;
         }
-        // 공 교환 시작 ~M
-        for (int i = 0; i < M; i++) {
-            StringTokenizer exchange = new StringTokenizer(br.readLine());
-            int firstBallNum = Integer.parseInt(exchange.nextToken()) - 1;
-            int secondBallNum = Integer.parseInt(exchange.nextToken()) - 1;
-            if (firstBallNum == secondBallNum) {
-                continue;
-            }
-            int firstBall = balls[firstBallNum];
-            int secondBall = balls[secondBallNum];
 
-            balls[firstBallNum] = secondBall;
-            balls[secondBallNum] = firstBall;
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken()) - 1;
+            int b = Integer.parseInt(st.nextToken()) - 1;
+
+            int temp = buckets[a];
+            buckets[a] = buckets[b];
+            buckets[b] = temp;
         }
 
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < N - 1; i++) {
-            sb.append(balls[i]).append(" ");
+        for (int bucket : buckets) {
+            sb.append(bucket).append(" ");
         }
-        sb.append(balls[N-1]);
-
-        System.out.println(sb.toString().trim());
-        br.close();
+        System.out.println(sb.toString());
     }
 }
