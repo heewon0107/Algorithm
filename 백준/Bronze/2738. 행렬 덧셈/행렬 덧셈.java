b3a1a047dad2eed8dae8fadd2e6127e131339526
Shin Hee Won <shinheewon0107@gmail.com>
2025-05-11 19:13:25 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 148 ms, Memory: 15756 KB -BaekjoonHub

---
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N, M;
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        int[][] arr = new int[N][M];
+
+        // initial arr
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        // add arr
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                 sb.append(arr[i][j] + Integer.parseInt(st.nextToken()))
+                         .append(" ");
+            }
+            if (i == N - 1) {
+                break;
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb);
+
+
+    }
+}
