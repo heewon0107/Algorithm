177330bb08945c5f91be9294259148094a0237b3
Shin Hee Won <shinheewon0107@gmail.com>
2025-06-09 22:30:18 +0900

    [Silver IV] Title: 보물, Time: 104 ms, Memory: 14340 KB -BaekjoonHub

---
+++ 백준/Silver/1026. 보물/보물.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] A = new int[N];
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(A);
+
+        st = new StringTokenizer(br.readLine());
+        Integer[] B = new Integer[N];
+        for (int i = 0; i < N; i++) {
+            B[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(B, Comparator.reverseOrder());
+
+        int result = 0;
+        for (int i = 0; i < N; i++) {
+            result += A[i] * B[i];
+        }
+        System.out.println(result);
+    }
+}
