c55a881474d179dcbf86834e48f903a15d55da0b
Shin Hee Won <shinheewon0107@gmail.com>
2025-09-26 23:26:29 +0900

    [level 3] Title: 합승 택시 요금, Time: 21.29 ms, Memory: 14.5 MB -BaekjoonHub

---
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.py
@@ -0,0 +1,35 @@
+import heapq
+
+def dijkstra(s, n, graph):
+    dijk = [1e9] * (n+1)
+    dijk[s] = 0
+    q = [(0, s)]
+    
+    while q:
+        fare, node = heapq.heappop(q)
+        # 이미 최소값
+        if dijk[node] < fare:
+            continue
+    
+        for next_fare, next_node in graph[node]:
+            new_fare = fare + next_fare
+            if new_fare < dijk[next_node]:
+                dijk[next_node] = new_fare
+                heapq.heappush(q, (new_fare, next_node))
+    return dijk
+       
+def solution(n, s, a, b, fares):
+    # 그래프 만들기
+    graph = [[] for _ in range(n+1)]
+    for start, end, fare in fares:
+        graph[start].append((fare, end))
+        graph[end].append((fare, start))
+    
+    dijk_s = dijkstra(s, n, graph) # 출발지부터 모든 노드에 가는 최단거리
+    dijk_a = dijkstra(a, n, graph) # a부터 모든 노드에 가는 최단거리
+    dijk_b = dijkstra(b, n, graph) # b부터 모든 노드에 가는 최단거리
+    
+    answer = 1e9
+    for i in range(1, n+1):
+        answer = min(answer, dijk_s[i] + dijk_a[i] + dijk_b[i])
+    return answer
