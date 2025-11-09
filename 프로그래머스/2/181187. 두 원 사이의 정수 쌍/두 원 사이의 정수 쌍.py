b8ce0caffea9c94f2959c84c3da1a7825ff25322
Shin Hee Won <shinheewon0107@gmail.com>
2025-11-09 12:20:59 +0900

    [level 2] Title: 두 원 사이의 정수 쌍, Time: 948.31 ms, Memory: 9.21 MB -BaekjoonHub

---
+++ 프로그래머스/2/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.py
@@ -0,0 +1,17 @@
+import math
+
+def solution(r1, r2):
+    answer = 0
+    for x in range(1, r2+1):
+        # y좌표 s~e
+        if x < r1:
+            s = math.ceil(math.sqrt(r1**2 - x**2))
+        else:
+            s = 0
+        
+        e = int(math.sqrt(r2**2 - x**2))
+        answer += e-s+1
+        
+        
+        
+    return answer*4
