a77928b140dd6943c6e86602d152c3010fcbcfb0
Shin Hee Won <shinheewon0107@gmail.com>
2026-02-02 11:57:02 +0900

    [Silver V] Title: 임스와 함께하는 미니게임, Time: 2440 ms, Memory: 43680 KB -BaekjoonHub

---
+++ 백준/Silver/25757. 임스와 함께하는 미니게임/임스와 함께하는 미니게임.py
@@ -0,0 +1,14 @@
+def solution():
+    N, game = input().split()
+    games = {
+        'Y': 1,
+        'F': 2,
+        'O': 3
+    }
+    N = int(N)
+    join = set()
+    for _ in range(N):
+        join.add(input())
+    print(len(join) // games[game])
+
+solution()
