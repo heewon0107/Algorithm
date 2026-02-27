76c90fe0b708e5ec8b49a4ec7ed965c876a3d134
Shin Hee Won <shinheewon0107@gmail.com>
2026-02-27 17:24:19 +0900

    [level 2] Title: 조건에 맞는 도서와 저자 리스트 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/144854. 조건에 맞는 도서와 저자 리스트 출력하기/조건에 맞는 도서와 저자 리스트 출력하기.sql
@@ -0,0 +1,13 @@
+SELECT  BOOK_ID,
+        AUTHOR_NAME,
+        SUBSTR(PUBLISHED_DATE, 1, 10) AS PUBLISHED_DATE
+        
+  FROM  BOOK B 
+        JOIN 
+        AUTHOR A 
+        ON B.AUTHOR_ID = A.AUTHOR_ID
+
+ WHERE  CATEGORY = '경제'
+ 
+ ORDER
+ BY     PUBLISHED_DATE
