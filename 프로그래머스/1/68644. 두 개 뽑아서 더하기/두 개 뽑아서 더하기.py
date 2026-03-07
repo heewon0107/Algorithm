458f8b2e8aac87cede0180e24eaeb6728f15bfd4
Shin Hee Won <shinheewon0107@gmail.com>
2026-03-07 16:31:02 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.63 ms, Memory: 9.19 MB -BaekjoonHub

---
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.py
@@ -0,0 +1,10 @@
+def solution(numbers):
+    answer = set()
+    N = len(numbers)
+    for i in range(N - 1):
+        for j in range(i+1, N):
+            answer.add(numbers[i] + numbers[j])
+    
+    answer = list(answer)
+    answer.sort()
+    return answer
