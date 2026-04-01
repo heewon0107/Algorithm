6e69f83b2853641d41eaaf87c3e590c8bc5f657f
Shin Hee Won <shinheewon0107@gmail.com>
2026-04-01 15:24:57 +0900

    [Gold V] Title: 가희와 은행, Time: 424 ms, Memory: 8300 KB -BaekjoonHub

--- 백준/Gold/22234. 가희와 은행/가희와 은행.cc
+++ 백준/Gold/22234. 가희와 은행/가희와 은행.cc
@@ -8,31 +8,32 @@ using namespace std;
 
 int main() {
     queue<pair<int, int>> waiting;
-
     priority_queue<
-        tuple<int,int,int>,
+        tuple<int, int, int>,
         vector<tuple<int,int,int>>,
         greater<tuple<int,int,int>>
-    > outside;
-
+        > outside;
+    
     int N, T, W, M;
     cin >> N >> T >> W;
-
+    
+    // Generate Waiting Queue
     for (int i = 0; i < N; i++) {
         int id, time;
         cin >> id >> time;
         waiting.push({id, time});
     }
-
+    
+    // Generate outside Queue
     cin >> M;
     for (int i = 0; i < M; i++) {
         int id, time, wait;
         cin >> id >> time >> wait;
         outside.push({wait, id, time});
     }
-
+    
+    // Start Process
     int clock = 0;
-
     while (clock < W) {
         auto [id, time] = waiting.front();
         waiting.pop();
@@ -42,20 +43,23 @@ int main() {
         for (int i = 0; i < work; i++) {
             cout << id << "\n";
             clock++;
-
-            while (!outside.empty() && get<0>(outside.top()) == clock) {
+            
+            // Insert Outsider into waiting.
+            while (!outside.empty()) {
                 auto [o_wait, o_id, o_time] = outside.top();
-                outside.pop();
-                waiting.push({o_id, o_time});
+                if (o_wait <= clock) {
+                    waiting.push({o_id, o_time});
+                    outside.pop();
+                } else {
+                    break;
+                }
             }
-
             if (clock >= W) return 0;
         }
-
+        // New Waiting
         if (time > T) {
             waiting.push({id, time - T});
         }
     }
-
     return 0;
 }
